89e7ba221f56abbc252039b8271c42f16edbb0ed
Chesley Brown <chezbrown@gmail.com>
2014-06-12 12:11:16 -0230

    Added grunt and jasmine test runner

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jasmine_node: {
+			options: {
+				extensions: 'js',
+				specNameMatcher: 'Spec'
+			},
+			test: [
+				'test/'
+			]
+		},
+		watch: {
+			test: {
+				files: ['lib/**/*.js', 'test/**/*Spec.js'],
+				tasks: ['jasmine_node:test']
+			}
+		}
+	});
+	
+	grunt.registerTask('test', ['jasmine_node:test', 'watch:test']);
+	grunt.registerTask('default', ['test']);
+};
